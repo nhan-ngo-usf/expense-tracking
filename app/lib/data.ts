358ad966fe804fc4d57c9e6a77b21844ab427868
Nhan <nhan.t.ngo@outlook.com>
2024-04-08 12:32:34 -0400

    =add dynamic rednering

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -60,6 +60,8 @@ export async function fetchCardData() {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
